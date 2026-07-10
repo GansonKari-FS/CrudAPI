b52a69c7041f712473a18c3e38db75e493fc6497
GansonKari-FS <kDganson@student.fullsail.edu>
2026-07-09 20:04:41 -0500

    yes

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+const studentRouter = require("./routes/students");
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL, {
+  // useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+// console log error if there is a connection error
+db.on(`error`, (error) => console.error(error));
+
+db.once(`open`, () => console.log(`Database connection established`));
+
+app.use(express.json());
+app.use("/students", studentRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
